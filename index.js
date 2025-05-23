e1e730715015675e56e0a48d1ba8b66060a8b466
korathak-736769 <korathank.k@gmail.com>
2025-05-23 11:46:05 +0700

    upadte edited

--- index.js
+++ index.js
@@ -6,28 +6,13 @@ app.get('/', (req, res) => {
     res.send('Hello Korathank PSRU!')
 })
 
-app.get('/product/:name/:price', (req, res) => {
-    const name = req.params.name
-    const price = req.params.price
-    res.send('Hello Product ' + name + ' ,Price ' + price)
-})
-app.get('/product/', (req, res) => {
-    // console.log(req.query);
-    const { name, price, detail } = req.query
-
-    res.send('Hello Product : ' + name + ',Price : ' + price + ' ,detial : ' + detail)
-})
-
-app.get('/product/computer', (req, res) => {
-    res.send('Hello product computer!')
-})
-
-app.get('/product/json', (req, res) => {
-    res.status(401).json({
-        name: 'computer',
-        price: 10000,
-        brand: 'ASUS'
-    })
+app.get('/products', (req, res) => {
+    const products = [
+        { id: 1, name: 'Product 1', price: 100 },
+        { id: 2, name: 'Product 2', price: 100 },
+        { id: 3, name: 'Product 3', price: 100 },
+    ]
+    res.json(products)
 })
 
 app.listen(port, () => {
