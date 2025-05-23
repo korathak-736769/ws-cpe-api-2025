3462b6f4717da5d7687490672efe1febe08dc94d
korathak-736769 <korathank.k@gmail.com>
2025-05-23 11:59:57 +0700

    upadte edited

--- index.js
+++ index.js
@@ -1,19 +1,12 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 const port = 3000
+const productRoute = require('./routes/productRoute')
 
-app.get('/', (req, res) => {
-    res.send('Hello Korathank PSRU!')
-})
+app.use(express.json()).use(morgan('dev'))
 
-app.get('/products', (req, res) => {
-    const products = [
-        { id: 1, name: 'Product 1', price: 100 },
-        { id: 2, name: 'Product 2', price: 100 },
-        { id: 3, name: 'Product 3', price: 100 },
-    ]
-    res.json(products)
-})
+app.use(productRoute)
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
