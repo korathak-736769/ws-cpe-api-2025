9217da16eecbc90fa611233dc5f4c801366c05f4
korathak-736769 <korathank.k@gmail.com>
2025-05-23 14:53:24 +0700

    upadte edited

--- controllers/productController.js
+++ controllers/productController.js
@@ -3,7 +3,7 @@ const ProductModel = require('../models/productModel')
 exports.addProduct = async (req, res) => {
     try {
         const { product_name, product_price } = req.body
-        if(!product_name || !product_price){
+        if (!product_name || !product_price) {
             return res.status(400).json({ message: 'Please provide all required fields [product_name, product_price]' })
         }
         const newProduct = new ProductModel({ product_name, product_price })
@@ -16,13 +16,22 @@ exports.addProduct = async (req, res) => {
 }
 
 exports.getAllProducts = async (req, res) => {
-    const products = [
-        { id: 1, name: 'Product 1', price: 100 },
-        { id: 2, name: 'Product 2', price: 100 },
-        { id: 3, name: 'Product 3', price: 100 },
-        { id: 4, name: 'Product 4', price: 100 },
-        { id: 5, name: 'Product 5', price: 100 },
-    ]
-    res.json(products)
+    try {
+        const products = await ProductModel.find()
+        res.status(200).json(products)
+    } catch (error) {
+        console.error('Error fetching products:', error)
+        res.status(500).json({ message: 'Internal server error' })
+    }
+}
+exports.getProductsById = async (req, res) => {
+    try {
+        const { id } = req.params
+        const products = await ProductModel.findById(id)
+        res.status(200).json(products)
+    } catch (error) {
+        console.error('Error fetching products:', error)
+        res.status(500).json({ message: 'Internal server error' })
+    }
 }
 
--- routes/productRoute.js
+++ routes/productRoute.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
-const { getAllProducts, addProduct } = require('../controllers/productController');
+const { getAllProducts, addProduct,getProductsById } = require('../controllers/productController');
 
 router.get('/getAllProducts', getAllProducts);
+router.get('/getProductsById/:id', getProductsById)
 router.post('/addProduct', addProduct)
 
 module.exports = router
